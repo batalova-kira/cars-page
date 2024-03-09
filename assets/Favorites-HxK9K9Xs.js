b65c8e6cd8586c339142f5880f58467ad992bd5d
batalova-kira <128981298+batalova-kira@users.noreply.github.com>
2024-03-09 15:09:38 +0000

    Deploying to gh-pages from @ batalova-kira/cars-page@2e401a99fde515e21922483069d3427da1c4d5ee 🚀

---
+++ assets/Favorites-HxK9K9Xs.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-F51HyJFq.js";const t=()=>r.jsx("div",{children:r.jsx("h2",{children:"Favorites"})});export{t as default};
---
+++ assets/HomePage-DEdFCaij.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-F51HyJFq.js";const t=()=>e.jsx(e.Fragment,{children:e.jsx("h1",{children:"Home page"})});export{t as default};
