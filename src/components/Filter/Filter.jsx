2811caceb8d6fa3b25c6b169bbbddf25feaa4942
batalova-kira <batalovakira@gmail.com>
2024-03-21 21:14:45 +0200

    add styles for dropdown

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -6,6 +6,7 @@ import {
     FilterWrapper,
     SearchBtn,
     SelectWrapper,
+    customStyles,
 } from "./Filter.styled";
 
 const MakesFilter = ({ onSelectMake }) => {
@@ -17,35 +18,6 @@ const MakesFilter = ({ onSelectMake }) => {
             label: option,
         };
     });
-
-    const customStyles = {
-        control: (provided, state) => ({
-            ...provided,
-            width: "224px",
-            border: "none",
-            borderRadius: "14px",
-            padding: "7px 8px",
-            fontSize: "18px",
-            lineHeight: "111%",
-            background: "#f7f7fb",
-            boxShadow: "none",
-            color: "#121417",
-        }),
-        placeholder: (provided, state) => ({
-            ...provided,
-            color: "#121417",
-        }),
-        option: (provided, state) => ({
-            ...provided,
-            backgroundColor: "#fff",
-            color: " rgba(18, 20, 23, 0.2)",
-        }),
-        indicatorSeparator: () => ({ display: "none" }),
-        dropdownIndicator: (provided, state) => ({
-            ...provided,
-            color: "#121417",
-        }),
-    };
 
     const handleChange = (selectedOption) => {
         setSelectedOption(selectedOption);
--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -39,3 +39,56 @@ export const SearchBtn = styled.button`
         background: #0b44cd;
     }
 `;
+
+export const customStyles = {
+    control: (provided, state) => ({
+        ...provided,
+        width: "224px",
+        border: "none",
+        borderRadius: "14px",
+        padding: "7px 8px",
+        fontSize: "18px",
+        lineHeight: "111%",
+        background: "#f7f7fb",
+        boxShadow: "none",
+        color: "#121417",
+    }),
+    placeholder: (provided, state) => ({
+        ...provided,
+        color: "#121417",
+    }),
+    option: (provided, state) => ({
+        ...provided,
+        backgroundColor: "#fff",
+        color: " rgba(18, 20, 23, 0.2)",
+        "&:hover": {
+            color: "#121417",
+        },
+    }),
+    indicatorSeparator: () => ({ display: "none" }),
+    dropdownIndicator: (provided, state) => ({
+        ...provided,
+        color: "#121417",
+    }),
+    menu: (provided) => ({
+        ...provided,
+        border: "1px solid rgba(18, 20, 23, 0.05)",
+        borderRadius: "14px",
+        boxShadow: "0 4px 36px 0 rgba(0, 0, 0, 0.02)",
+        background: "#fff",
+        overflow: "hidden",
+        padding: "14px 8px",
+    }),
+    menuList: (provided) => ({
+        ...provided,
+        "&::-webkit-scrollbar": {
+            width: "8px",
+            height: "130px",
+        },
+        "&::-webkit-scrollbar-thumb": {
+            backgroundColor: "rgba(18, 20, 23, 0.05)",
+            borderRadius: "10px",
+            margin: "14px",
+        },
+    }),
+};
